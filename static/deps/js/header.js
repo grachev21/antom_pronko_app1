052feb89d0afdb056f55ae5c78d0b2f63f2ce35f
grachev21 <grachev613@gmail.com>
2024-03-26 21:45:54 +0400

    11234

--- static/deps/js/header.js
+++ static/deps/js/header.js
@@ -1,37 +1,62 @@
-var navbar = document.querySelector(".navbar-brand");
-var block = document.querySelector(".show_home_block")
-navbar.addEventListener("mouseenter", showHome);
-navbar.addEventListener("mouseout", hideHome);
-block.addEventListener("mouseenter", blockShowHome);
-block.addEventListener("mouseleave", blockHideHome);
+window.onload = function donenow() {
 
-function showHome(el) {
+    // document.querySelector("body").onmouseover = function (event) {
+    //     let target = event.target
+    //     show(target.className)
+    // }
+
+    // function show(el) {
+    //     // let check = document.querySelector(".show_home_block").style.top
+    //     if (el == "link_home") {
+    //         // console.log(check)
+    //         show_block()
+    //     }
+
+    //     // if (el != "link_home" && el != "home" && el != "show_home_block" && el != "show_content") {
+    //     //     hide_block()
+    //     // }
+    // }
+
+    document.querySelector(".home").addEventListener("mouseenter",
+        function () { show_block(this); });
+
+    function show_block(el) {
+
+        let start = 330;
+        console.log("xxxxxxx")
+
+
+        let block = document.querySelector('.show_home_block');
+        block.classList.add("finish_home_block")
+
+
+        // setInterval(function () {
+        //     if (start === 280) {
+        //         clearInterval();
+        //     } else {
+        //         start--;
+        //         document.querySelector(".show_home_block").style.top = `${start}px`;
+        //     }
+        // }, 2);
+    }
+
+}
+
+function hide_block() {
+    console.log("no")
+    document.querySelector('.show_content').style.opacity = "0%";
+
     let block = document.querySelector('.show_home_block');
-    let x = 330;
-
-
-        block.style.display = "block";
-        block.style.top = "330px";
-
-        setInterval(function () {
-            if (x === 260) {
-                clearInterval();
-            } else {
-                x--;
-                block.style.top = `${x}px`;
-            }
-        }, 2);
-
+    let start = 290;
+    document.querySelector('.show_content').style.opacity = "0%";
+    document.querySelector('.show_content').style.transition = "1s";
+    setInterval(function () {
+        if (start === 330) {
+            clearInterval();
+        } else {
+            start++;
+            block.style.top = `${start}px`;
+        }
+    }, 18);
 }
 
-function hideHome(el) {
-    document.querySelector(".show_home_block").style.display = "none"
-}
-
-function blockShowHome(el) {
-    document.querySelector(".show_home_block").style.display = "block"
-}
-
-function blockHideHome(el) {
-    document.querySelector(".show_home_block").style.display = "none"
-}
