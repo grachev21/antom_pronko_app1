9d850411f21363569bf4671925fb33b0555aeef8
grachev21 <grachev613@gmail.com>
2024-03-28 17:56:51 +0400

    Боковая панель

--- js/header.js
+++ js/header.js
@@ -6,22 +6,17 @@ window.onload = function donenow() {
     }
 
     function show(el) {
-        console.log(el)
         if (el == "link_home") {
             document.querySelector('.show_home_block').classList.remove("block_deactivate");
             document.querySelector('.show_home_block').classList.add("block_activate");
         }
 
-
         let list_class = ["show_home_block", "block_img", "img"]
         if (el == "background" || el == "block_content" || el == "info_text" || el == "block-content") {
-            console.log("deactive");
             document.querySelector('.show_home_block').classList.remove("block_activate");
             document.querySelector('.show_home_block').classList.add("block_deactivate");
         }
         // console.log(document.querySelector(".show_home_block").className)
     }
-
-
 
 }
