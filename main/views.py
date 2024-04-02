085a1c0e454b7910a43f401293d76ce90ab39ea0
grachev21 <grachev613@gmail.com>
2024-04-02 11:26:16 +0400

    12341234

---
+++ main/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+
+
+class Home(ListView):
+    model = "x"
+    template_name = 'main/base.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context[""] = 
+        return context
+    
+
+
+
+
